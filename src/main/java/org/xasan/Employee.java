29d9779a8b92a983fcb0cdac19040f1c82e83852
Khasanjon <122252549+khasanjontuychiboev@users.noreply.github.com>
2023-12-27 19:18:45 +0500

    Update  3 Employee.java

--- src/main/java/org/xasan/Employee.java
+++ src/main/java/org/xasan/Employee.java
@@ -3,8 +3,10 @@ package org.xasan;
 public class Employee {
     public static void main(String[] args) {
 
-        System.out.println("salom");
-         System.out.println("salom");
+        System.out.println("Hello");
+        System.out.println("Hello");
+        System.out.println("Hello");
+        System.out.println("Hello");
 
     }
 }
