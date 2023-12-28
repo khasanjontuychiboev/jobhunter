f74ced89f3bde6a790eed67af1229999787c6c80
khasanjontuychiboev <khasanjontuychiboev@gmail.com>
2023-12-28 10:58:05 +0500

    xal boldi

--- src/main/java/org/xasan/Employee.java
+++ src/main/java/org/xasan/Employee.java
@@ -2,9 +2,7 @@ package org.xasan;
 
 public class Employee {
     public static void main(String[] args) {
-        if (true){
-            System.out.println("always true");
-        }
+
 
     }
 }
