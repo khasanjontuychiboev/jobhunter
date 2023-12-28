a39fa915bc81cd673f451ad58d1355c6d3879ec2
khasanjontuychiboev <khasanjontuychiboev@gmail.com>
2023-12-28 13:14:18 +0500

    feature commmit2

--- src/main/java/org/xasan/Employee.java
+++ src/main/java/org/xasan/Employee.java
@@ -2,10 +2,9 @@ package org.xasan;
 
 public class Employee {
     public static void main(String[] args) {
-
-
          System.out.println("salom REBASE 2");
-        System.out.println("salom commit 1");
+        System.out.println("feature-a commit 1");
+        System.out.println("feature-a commit 2");
 
     }
 }
