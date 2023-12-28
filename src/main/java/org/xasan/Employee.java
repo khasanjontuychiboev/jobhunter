5b141edcdc830ad3a0574e5c6c2af321f7c2f13a
Khasanjon <122252549+khasanjontuychiboev@users.noreply.github.com>
2023-12-28 14:46:51 +0500

    Feature a (#2)
    * feature commmit1
    
    * Yangi. Update Employee.java
    
    Shifudna
    
    * feature commmit2
    
    * feature commmit3
    
    * Reabse-2 for branch feature-a
    
    ---------
    
    Co-authored-by: ustoz123 <154898530+ustoz123@users.noreply.github.com>

--- src/main/java/org/xasan/Employee.java
+++ src/main/java/org/xasan/Employee.java
@@ -2,10 +2,10 @@ package org.xasan;
 
 public class Employee {
     public static void main(String[] args) {
-
-
-        System.out.println("salom REBASE 1");
-         System.out.println("salom REBASE 2");
+        System.out.println("REBASE 2");
+        System.out.println("feature-a commit 1");
+        System.out.println("feature-a commit 2");
+        System.out.println("feature-a commit 3");
 
     }
 }
