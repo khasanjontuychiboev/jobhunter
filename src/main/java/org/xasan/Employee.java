774441accdb282ed97d7a163f6e7c0335bf50992
Khasanjon <122252549+khasanjontuychiboev@users.noreply.github.com>
2023-12-28 10:32:54 +0500

    Update Employee.java

--- src/main/java/org/xasan/Employee.java
+++ src/main/java/org/xasan/Employee.java
@@ -4,13 +4,6 @@ public class Employee {
     public static void main(String[] args) {
 
         System.out.println("salom");
-         System.out.println("salom");
-        System.out.println("salom");
-        System.out.println("salom");
-        System.out.println("Hello");
-        System.out.println("Hello");
-        System.out.println("Hello");
-        System.out.println("Hello");
 
     }
 }
