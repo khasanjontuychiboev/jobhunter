a79dccc5ec9f24a9e048316a9425763a180dfb85
ustoz123 <154898530+ustoz123@users.noreply.github.com>
2023-12-28 11:40:21 +0500

    Yangi. Update Employee.java
    Shifudna

--- src/main/java/org/xasan/Employee.java
+++ src/main/java/org/xasan/Employee.java
@@ -3,8 +3,8 @@ package org.xasan;
 public class Employee {
     public static void main(String[] args) {
 
+
          System.out.println("salom REBASE 2");
-        System.out.println("salom REBASE");
         System.out.println("salom commit 1");
 
     }
