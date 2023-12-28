d21d6fab7251ca5d8f517db5197b96249d94f2f9
Khasanjon <122252549+khasanjontuychiboev@users.noreply.github.com>
2023-12-28 13:25:20 +0500

    Reabse 2 Update Employee.java
    REabse 2

--- src/main/java/org/xasan/Employee.java
+++ src/main/java/org/xasan/Employee.java
@@ -4,7 +4,8 @@ public class Employee {
     public static void main(String[] args) {
 
 
-        System.out.println("salom REBASE");
+        System.out.println("salom REBASE 1");
+         System.out.println("salom REBASE 2");
 
     }
 }
